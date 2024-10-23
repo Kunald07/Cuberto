8c8fcaf9bd3753eb19e3a47018e8ad14a01b0cd4
Kunal Deshmukh <deshmukh1207@gmail.com>
2024-10-23 23:38:13 +0530

    Cuberto - Riyadh
    Official website of Riyadh,
    Saudi Arabia's capital - Clone Website

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#container"),
+    smooth: true,
+    multiplier: 1.5,
+    smartphone: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
+});
